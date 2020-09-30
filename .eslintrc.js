6c720c3f8242bd3475b5a49add26d94484c92f7e
Anm520 <>
2020-09-30 11:19:06 +0800

    初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,14 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/no-unused-vars': 'warn',
+    semi: 0, // 末尾分号
+    'comma-dangle': [0, 'always-multiline'], // 数组和对象键值对最后一个逗号， never参数：不能带末尾的逗号, always参数：必须带末尾的逗号，always-multiline：多行模式必须带逗号，单行模式不能带逗号
+    'space-before-function-paren': 0,
+    'prefer-const': 0,
+    'no-unused-vars': 0,
+    'vue/no-unused-components': 0,
+  },
+};
---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+  name: '',
+  setup() {},
+};
+</script>
+<style lang='scss' scoped>
+</style>
